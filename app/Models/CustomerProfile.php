d50966ce78a8b362d9671da34b959bf1dc281070
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 15:17:42 +0600

    relation made [ Profile -> user ]

--- app/Models/CustomerProfile.php
+++ app/Models/CustomerProfile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CustomerProfile extends Model
 {
@@ -28,4 +29,8 @@ class CustomerProfile extends Model
         'user_id',
     ];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
