5f87cc6c1dd2cae3b79d3816e292c785721db6b7
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 15:09:31 +0600

    Invoice migrations & Model Updated

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'total',
+        'vat',
+        'payable',
+        'cus_details',
+        'ship_details',
+        'tran_id',
+        'val_id',
+        'delivery_status',
+        'payment_status',
+        'customer_id'
+    ];
 }
--- database/migrations/2024_09_01_072349_create_invoices_table.php
+++ database/migrations/2024_09_01_072349_create_invoices_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,16 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total', 8, 2);
+            $table->decimal('vat', 8, 2);
+            $table->decimal('payable', 8, 2);
+            $table->string('cus_details', 500);
+            $table->string('ship_details', 500);
+            $table->string('tran_id', 100);
+            $table->string('val_id', 100)->default(0);
+            $table->enum('delivery_status', ['Pending', 'Processing', 'Completed']);
+            $table->string('payment_status');
+            $table->foreignId('customer_id')->references('id')->on('customer_profiles')->cascadeOnUpdate()->restrictOnDelete();
             $table->timestamps();
         });
     }
