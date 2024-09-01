39df004d7e5159596219edfb27441a2d5e77003f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 16:05:25 +0600

    relation made [ Wishlist  -> Product, customerProfile ]

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Wishlist extends Model
 {
@@ -12,4 +13,14 @@ class Wishlist extends Model
         'customer_id',
         'product_id',
     ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function customerProfile(): BelongsTo
+    {
+        return $this->belongsTo(CustomerProfile::class, 'customer_id');
+    }
 }
