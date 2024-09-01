b43b62f65ba7d15748480fc55a32d7f4c8cc91d1
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 15:10:38 +0600

    ProductReview migrations & Model Updated

--- app/Models/ProductReview.php
+++ app/Models/ProductReview.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductReview extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'description',
+        'rating',
+        'product_id',
+        'customer_id',
+    ];
 }
--- database/migrations/2024_09_01_071850_create_product_reviews_table.php
+++ database/migrations/2024_09_01_071850_create_product_reviews_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('product_reviews', function (Blueprint $table) {
             $table->id();
+            $table->string('description', 1000);
+            $table->string('rating', 10);
+            $table->foreignId('product_id')->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('customer_id')->references('id')->on('customer_profiles')->cascadeOnUpdate()->restrictOnDelete();
             $table->timestamps();
         });
     }
