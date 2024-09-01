ae49ff4ccdeb6c6360611651445a19b5c70b540a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 15:11:22 +0600

    sslcommerz migrations & Model Updated

--- app/Models/SslcommerzCredential.php
+++ app/Models/SslcommerzCredential.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class SslcommerzCredential extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'store_id',
+        'store_passwd',
+        'currency',
+        'success_url',
+        'fail_url',
+        'cancel_url',
+        'ipn_url',
+        'init_url',
+    ];
 }
--- database/migrations/2024_09_01_072243_create_sslcommerz_credentials_table.php
+++ database/migrations/2024_09_01_072243_create_sslcommerz_credentials_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,14 @@ return new class extends Migration
     {
         Schema::create('sslcommerz_credentials', function (Blueprint $table) {
             $table->id();
+            $table->string('store_id')->nullable();
+            $table->string('store_passwd')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('success_url')->nullable();
+            $table->string('fail_url')->nullable();
+            $table->string('cancel_url')->nullable();
+            $table->string('ipn_url')->nullable();
+            $table->string('init_url')->nullable();
             $table->timestamps();
         });
     }
