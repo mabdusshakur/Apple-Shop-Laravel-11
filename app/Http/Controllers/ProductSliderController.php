57804d6b6ad40fdc38b9afeb5153fee7326983f4
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 13:19:12 +0600

    ProductSlider model,migration,controller,request added

---
+++ app/Http/Controllers/ProductSliderController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductSlider;
+use App\Http\Requests\StoreProductSliderRequest;
+use App\Http\Requests\UpdateProductSliderRequest;
+
+class ProductSliderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductSliderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductSlider $productSlider)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductSlider $productSlider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductSliderRequest $request, ProductSlider $productSlider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductSlider $productSlider)
+    {
+        //
+    }
+}
