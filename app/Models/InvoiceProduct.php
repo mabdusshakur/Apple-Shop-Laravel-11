302bd072fc7ef59d68e2cefa11ced97330e7fe7b
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 16:10:28 +0600

    relation made [ InvoiceProduct  -> product, invoice ]

--- app/Models/InvoiceProduct.php
+++ app/Models/InvoiceProduct.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class InvoiceProduct extends Model
 {
@@ -14,4 +15,14 @@ class InvoiceProduct extends Model
         'product_id',
         'invoice_id',
     ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function invoice(): BelongsTo
+    {
+        return $this->belongsTo(Invoice::class);
+    }
 }
