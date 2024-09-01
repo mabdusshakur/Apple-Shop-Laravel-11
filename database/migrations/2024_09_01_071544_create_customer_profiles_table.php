fdc8714f153b1fb1364d39204469fe4008c67af1
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 14:15:14 +0600

    CustomerProfile migrations & Model Updated

--- database/migrations/2024_09_01_071544_create_customer_profiles_table.php
+++ database/migrations/2024_09_01_071544_create_customer_profiles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,22 @@ return new class extends Migration
     {
         Schema::create('customer_profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('cus_name', 100);
+            $table->string('cus_add', 500);
+            $table->string('cus_city', 50);
+            $table->string('cus_state', 50);
+            $table->string('cus_postcode', 50);
+            $table->string('cus_country', 50);
+            $table->string('cus_phone', 50);
+            $table->string('cus_fax', 50);
+            $table->string('ship_name', 100);
+            $table->string('ship_add', 100);
+            $table->string('ship_city', 100);
+            $table->string('ship_state', 100);
+            $table->string('ship_postcode', 100);
+            $table->string('ship_country', 100);
+            $table->string('ship_phone', 50);
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->restrictOnDelete();
             $table->timestamps();
         });
     }
