7813e2ae69d6db4a7b87def650a634bf95d23a92
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 16:03:19 +0600

    relation made [ ProductSlider  -> Product  ]

--- app/Models/ProductSlider.php
+++ app/Models/ProductSlider.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductSlider extends Model
 {
@@ -16,4 +17,9 @@ class ProductSlider extends Model
         'active',
         'product_id',
     ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
