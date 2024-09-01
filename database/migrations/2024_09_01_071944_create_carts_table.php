e0ff00580a5407792346c93adf1a4e71594edc35
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-01 15:09:11 +0600

    Card migrations & Model Updated

--- database/migrations/2024_09_01_071944_create_carts_table.php
+++ database/migrations/2024_09_01_071944_create_carts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->string('color', 200);
+            $table->string('size', 200);
+            $table->integer('quantity');
+            $table->foreignId('product_id')->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('customer_id')->references('id')->on('customer_profiles')->cascadeOnUpdate()->restrictOnDelete();
             $table->timestamps();
         });
     }
