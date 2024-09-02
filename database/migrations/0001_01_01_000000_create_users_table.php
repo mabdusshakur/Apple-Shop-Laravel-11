c9744dfea7129b7f203fe06e9bb4543cb72bcd50
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-02 10:04:15 +0600

    is_admin field added on users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email', 50)->unique();
+            $table->boolean('is_admin')->default(false);
             $table->integer('otp');
             $table->timestamps();
         });
