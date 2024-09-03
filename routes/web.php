b99822250256aaa7899ee59152bd917a1430b9f3
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-03 18:13:08 +0600

    ssl routes added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\V1\SSLCommerzController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::post('/ssl-payment-success', [SSLCommerzController::class, 'paymentSuccess']);
+Route::post('/ssl-payment-fail', [SSLCommerzController::class, 'paymentFail']);
+Route::post('/ssl-payment-cancel', [SSLCommerzController::class, 'paymentCancel']);
