28002e8316fff45f0b9ba1343dd3fd1171151c58
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-04 22:34:42 +0600

    admin frontend controller added

---
+++ app/Http/Controllers/AdminFrontController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminFrontController extends Controller
+{
+    function dashboardPage()
+    {
+        return view('admin.dashboard.dashboard-page');
+    }
+
+    function productPage()
+    {
+        return view('admin.dashboard.product-page');
+    }
+
+    function categoryPage()
+    {
+        return view('admin.dashboard.category-page');
+    }
+}
