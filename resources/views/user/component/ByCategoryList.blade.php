df7e82c75fc8167268075d2b05ae3e91d0e9e1d8
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-04 13:57:07 +0600

    product_details id passed

--- resources/views/user/component/ByCategoryList.blade.php
+++ resources/views/user/component/ByCategoryList.blade.php
@@ -47,13 +47,13 @@
                                         </a>
                                         <div class="product_action_box">
                                             <ul class="list_none pr_action_btn">
-                                                <li><a href="/product-details?id=${item['id']}" class="popup-ajax"><i class="icon-magnifier-add"></i></a></li>
+                                                <li><a href="/product-details?id=${item['product_detail']['id']}" class="popup-ajax"><i class="icon-magnifier-add"></i></a></li>
 
                                             </ul>
                                         </div>
                                     </div>
                                     <div class="product_info">
-                                        <h6 class="product_title"><a href="/product-details?id=${item['id']}">${item['title']}</a></h6>
+                                        <h6 class="product_title"><a href="/product-details?id=${item['product_detail']['id']}">${item['title']}</a></h6>
                                         <div class="product_price">
                                             <span class="price">$ ${item['price']}</span>
                                         </div>
