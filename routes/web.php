e77105cd01c75e9bcf0b43383475c6241c0c98b7
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-04 18:05:06 +0600

    added web routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,25 @@
 <?php
 
 use App\Http\Controllers\Api\V1\SSLCommerzController;
+use App\Http\Controllers\UserFrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+
+
+
+
+
+Route::get('/', [UserFrontController::class, 'homePage'])->name('web.home');
+Route::get('/login', [UserFrontController::class, 'loginPage'])->name('web.login');
+Route::get('/verify-otp', [UserFrontController::class, 'verifyOtpPage'])->name('web.verify-otp');
+Route::get('/profile', [UserFrontController::class, 'profilePage'])->name('web.profile');
+Route::get('/wish-list', [UserFrontController::class, 'wishListPage'])->name('web.wish-list');
+Route::get('/cart-list', [UserFrontController::class, 'cartListPage'])->name('web.cart-list');
+Route::get('/product-details/{id?}', [UserFrontController::class, 'productDetailsPage'])->name('web.product-details');
+Route::get('/policy', [UserFrontController::class, 'policyPage'])->name('web.policy');
+Route::get('/product-by-brand/{id?}', [UserFrontController::class, 'productByBrandPage'])->name('web.product-by-brand');
+Route::get('/product-by-category/{id?}', [UserFrontController::class, 'productByCategoryPage'])->name('web.product-by-category');
 
 
 Route::post('/ssl-payment-success', [SSLCommerzController::class, 'paymentSuccess']);
